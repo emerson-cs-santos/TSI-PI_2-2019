33408b872accf24622dab77255c34c1cf16180ab
Emerson Costa <syphonemerson@yahoo.com.br>
2019-09-19 16:10:21 -0300

    Adicionado campos que faltavam.

--- SQL/produtos.sql
+++ SQL/produtos.sql
@@ -1,7 +1,16 @@
 -- TABELA DE PRODUTOS
 CREATE TABLE IF NOT EXISTS PRODUTOS 
 	(
-		codigo	INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-		,nome	VARCHAR(30) NOT NULL
-		,tipo	VARCHAR(20) NOT NULL
+		codigo		INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
+		,nome		VARCHAR(30) NOT NULL
+		,descri		VARCHAR(50) NOT NULL
+		,categoria	VARCHAR(20) NOT NULL
+		,imagem		blob NOT NULL
+		,preco		decimal(8,2) NOT NULL
+		,desconto	decimal(8,2) NOT NULL
+		,estoque	INTEGER NOT NULL
+		,tipo		VARCHAR(20) NOT NULL -- 'ATIVO'
+		,ean		VARCHAR(20) NOT NULL
 	);
+
+-- insert into PRODUTOS (codigo,nome,descri,categoria,imagem,preco,desconto,estoque,tipo,ean) values (0,'teste','desc','cate','',25.5,2.5,14,'Ativo','eannn');
