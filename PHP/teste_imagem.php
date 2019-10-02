e6d77ba569a2feb634f10f52ab76feff1fabd8a9
Emerson Costa <syphonemerson@yahoo.com.br>
2019-10-02 13:54:16 -0300

    continuando imagem

--- PHP/teste_imagem.php
+++ PHP/teste_imagem.php
@@ -1,5 +1,8 @@
 <?php
 
+return $_POST['arquivo'];
+return $_FILES["myFile"]["name"];
+
 // MODO POST
 $url = $_POST['url'];
 
@@ -13,5 +16,21 @@ $resposta = 'ok';
 // RETORNA RESULTADO
 echo $resposta;
 return;
+
+
+// $arquivo_temp   = $_FILES["certificado"]["tmp_name"];
+// $nome_arquivo   = $_FILES["certificado"]["name"];
+// $tamanho        = $_FILES["certificado"]["size"];
+
+// $extensao       = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
+
+
+//  $novoNome       = $nome_arquivo;
+ 
+ 
+//  if(move_uploaded_file($arquivo_temp, [[NOME_DA_PASTA_DESTINO]].$novoNome)){
+// 	echo true;
+//  }
+ 
 
 ?>
