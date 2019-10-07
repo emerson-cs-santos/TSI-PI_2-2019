b36b2dab42b6a0df5c15f7eeb3c92656126a8180
Emerson Costa <syphonemerson@yahoo.com.br>
2019-10-07 10:14:22 -0300

    Imagem continuação

--- PHP/imagem.php
+++ PHP/imagem.php
@@ -1,53 +1,58 @@
 <?php
-$destino = 'imagens/' . $_FILES['myFile']['name'];
+// $destino = '../imagens/' . $_FILES['myFile']['name'];
  
-$arquivo_tmp = $_FILES['myFile']['tmp_name'];
+// $arquivo_tmp = $_FILES['myFile']['tmp_name'];
 
-move_uploaded_file( $arquivo_tmp, $destino  );
+// move_uploaded_file( $arquivo_tmp, $destino  );
 
 // CONECTAR AO BANCO DE DADOS E SALVAR CAMINHO GERADO DA IMAGEM
 
+// Controlar o fluxo de imagens
+	// Inserir
+	// Atualizar
+	// Deletar
 
-// verifica se foi enviado um arquivo
-// if ( isset( $_FILES[ 'arquivo' ][ 'name' ] ) && $_FILES[ 'arquivo' ][ 'error' ] == 0 ) {
-    // echo 'Você enviou o arquivo: <strong>' . $_FILES[ 'arquivo' ][ 'name' ] . '</strong><br />';
-    // echo 'Este arquivo é do tipo: <strong > ' . $_FILES[ 'arquivo' ][ 'type' ] . ' </strong ><br />';
-    // echo 'Temporáriamente foi salvo em: <strong>' . $_FILES[ 'arquivo' ][ 'tmp_name' ] . '</strong><br />';
-    // echo 'Seu tamanho é: <strong>' . $_FILES[ 'arquivo' ][ 'size' ] . '</strong> Bytes<br /><br />';
+
+//verifica se foi enviado um arquivo
+if ( isset( $_FILES[ 'myFile' ][ 'name' ] ) && $_FILES[ 'myFile' ][ 'error' ] == 0 ) {
+    echo 'Você enviou o arquivo: <strong>' . $_FILES[ 'myFile' ][ 'name' ] . '</strong><br />';
+    echo 'Este arquivo é do tipo: <strong > ' . $_FILES[ 'myFile' ][ 'type' ] . ' </strong ><br />';
+    echo 'Temporáriamente foi salvo em: <strong>' . $_FILES[ 'myFile' ][ 'tmp_name' ] . '</strong><br />';
+    echo 'Seu tamanho é: <strong>' . $_FILES[ 'myFile' ][ 'size' ] . '</strong> Bytes<br /><br />';
  
-    // $arquivo_tmp = $_FILES[ 'arquivo' ][ 'tmp_name' ];
-    // $nome = $_FILES[ 'arquivo' ][ 'name' ];
+    $arquivo_tmp = $_FILES[ 'myFile' ][ 'tmp_name' ];
+    $nome = $_FILES[ 'myFile' ][ 'name' ];
  
-    // Pega a extensão
-//    $extensao = pathinfo ( $nome, PATHINFO_EXTENSION );
+  //  Pega a extensão
+   $extensao = pathinfo ( $nome, PATHINFO_EXTENSION );
  
-    // Converte a extensão para minúsculo
- //   $extensao = strtolower ( $extensao );
+    //Converte a extensão para minúsculo
+   $extensao = strtolower ( $extensao );
  
-    // Somente imagens, .jpg;.jpeg;.gif;.png
-    // Aqui eu enfileiro as extensões permitidas e separo por ';'
-    // Isso serve apenas para eu poder pesquisar dentro desta String
- //   if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
-        // Cria um nome único para esta imagem
-        // Evita que duplique as imagens no servidor.
-        // Evita nomes com acentos, espaços e caracteres não alfanuméricos
-    //    $novoNome = uniqid ( time () ) . '.' . $extensao;
+    //Somente imagens, .jpg;.jpeg;.gif;.png
+    //Aqui eu enfileiro as extensões permitidas e separo por ';'
+    //Isso serve apenas para eu poder pesquisar dentro desta String
+   if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
+      //  Cria um nome único para esta imagem
+       // Evita que duplique as imagens no servidor.
+        //Evita nomes com acentos, espaços e caracteres não alfanuméricos
+       $novoNome = uniqid ( time () ) . '.' . $extensao;
  
-        // Concatena a pasta com o nome
-    //    $destino = 'imagens / ' . $novoNome;
+        //Concatena a pasta com o nome
+       $destino = '../imagens / ' . $novoNome;
  
-        // tenta mover o arquivo para o destino
-     //   if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
-     //       echo 'Arquivo salvo com sucesso em : <strong>' . $destino . '</strong><br />';
-          //  echo ' < img src = "' . $destino . '" />';
-     //   }
-    //    else
-    //        echo 'Erro ao salvar o arquivo. Aparentemente você não tem permissão de escrita.<br />';
-  //  }
- //   else
- //       echo 'Você poderá enviar apenas arquivos "*.jpg;*.jpeg;*.gif;*.png"<br />';
-//}
-//else
- //   echo 'Você não enviou nenhum arquivo!';
+        //tenta mover o arquivo para o destino
+       if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
+           echo 'Arquivo salvo com sucesso em : <strong>' . $destino . '</strong><br />';
+           echo ' < img src = "' . $destino . '" />';
+       }
+       else
+           echo 'Erro ao salvar o arquivo. Aparentemente você não tem permissão de escrita.<br />';
+   }
+   else
+       echo 'Você poderá enviar apenas arquivos "*.jpg;*.jpeg;*.gif;*.png"<br />';
+}
+else
+   echo 'Você não enviou nenhum arquivo!';
 
 ?>
