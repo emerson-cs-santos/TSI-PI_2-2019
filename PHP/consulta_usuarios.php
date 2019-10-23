0884d443bc9a1154ef8155324d757fa84e7aed51
Emerson Costa <syphonemerson@yahoo.com.br>
2019-10-23 09:50:50 -0300

    Correção no uso do ajax
    O retorno da tabela não deve refazer a página, pois foge da lógica do AJAX, deve apenas retornar e refazer a tabela.
    Para isso fiz um segundo ajax apenas retornando a tabela, mas fiz usando Jquery.

---
+++ PHP/consulta_usuarios.php
@@ -0,0 +1,50 @@
+<?php
+    include('../PHP/sessao.php');
+	$codigo = $_POST['codigo'];
+	
+	include('..\PHP\conexao_bd.php');
+	
+	$query = "select * from usuarios order by codigo desc";
+	$result = $conn->query($query);
+		
+	echo "<div id='table' class='container'>";
+	echo "<div class='row-fluid'>";
+	
+		echo "<div class='col-xs-6'>";
+		echo "<div class='table-responsive'>";
+		
+			echo "<table id ='usuarios_table' class='table table-hover table-inverse'>";
+			
+			echo "<tr>";
+			echo "<th>Codigo</th>";
+			echo "<th>Login</th>";
+			echo "<th>Status</th>";
+			echo "<th>Alterar</th>";
+			echo "<th>Desativar</th>";
+			echo "<th>Deletar</th>";
+			echo "</tr>";
+			
+			echo "<tbody>";
+	
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+						
+					echo "<tr>";
+					echo "<td>" . $row["codigo"] . "</td>";
+					echo "<td>" . $row["nome"] . "</td>";
+					echo "<td>" . $row["tipo"] . "</td>";
+					
+					echo " <td> <a id='' type='button' class='btn btn-primary btn-lg' href='Usuarios_digitar.php?ID={$row["codigo"]}'>Alterar</a> </td>";
+					echo " <td> <a id='' type='button' class='btn btn-primary btn-lg' onclick='desativar({$row["codigo"]})' >Desativar</a> </td>";
+					echo " <td> <a id='' type='button' class='btn btn-primary btn-lg' onclick='deletar({$row["codigo"]})' >Deletar</a> </td>";
+
+					echo "</tr>";			
+				}
+			} else {
+				echo "0 results";
+			}
+			
+		echo "</tbody>";
+		echo "</table>";
+?>
