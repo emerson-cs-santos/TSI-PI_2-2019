8197f3d9815bdfe830764e49ad5e8edba218b2c9
Emerson Costa <syphonemerson@yahoo.com.br>
2019-10-28 09:47:03 -0300

    continuando e-mail

--- JS/reset_email.js
+++ JS/reset_email.js
@@ -34,66 +34,88 @@ function login_verif()
 
 function enviar_email()
 {
-   
     var parametros = '';
     var email = '';
-    var reposta = '';
+    var resposta = '';
+    var login = '';
 
     // Gera e grava código de reset do e-mail
     // Retorna endereço de e-mail do login
-    parametros = "login=" + document.getElementById('login_usuario_reset').value;  
+    login = document.getElementById('login_usuario_reset').value; 
+    parametros = "login=" + login;  
     $.post('PHP/cod_reset_email.php',parametros, function(data)
         {
-            
-            reposta = JSON.parse(data);
-        }
-    )       
+            // Converter array do php em objeto
+            resposta = JSON.parse(data);
 
-    switch (reposta.status)
-    {
-        
-        case 'ok':
-            cod_random = reposta.cod_random;
-            email = reposta.email;
-        break; 
-
-        case 'nao':
-            swal(
-                {
-                    title:  "Usuário não encontrado!",
-                    text:   "Por favor entrar em contato com o administrador do sistema!",
-                    icon:   "warning",
-                    button: "OK",
-                }
-            )                    
-            // Marcar passo como Pendente
-            passos_reset_email('2','Pendente');
-            return false;               
-            
-        default:
-        swal(
+            switch (resposta.status)
             {
-                title:  "Problemas ao encontrar usuário!",
-                text:   "Por favor entrar em contato com o administrador do sistema!",
-                icon:   "error",
-                button: "OK",
-            }
-        )
-        passos_reset_email('2','Pendente');
-        return false;               
-    }    
-    
+                
+                case 'ok':
+                    cod_random = resposta.cod_random;
+                    email = resposta.email;
 
-    parametros = '';
-    // Ajax com Jquery e está refazendo apenas a tabela 
-    $.post('PHP/PHPMailer.php',parametros, function(data)
-        {
-            alert(data);
+                    // Envio de e-mail
+                    parametros = "cod_random=" + cod_random + "&email=" + email + "&login=" + login;
+                    // Ajax com Jquery e está refazendo apenas a tabela 
+                    $.post('PHP/PHPMailer.php',parametros, function(data)
+                        {
+                            if(data=='ok')
+                            {
+                                passos_reset_email('2','OK');  
+                                swal(
+                                    {
+                                        title:  "Mensagem enviada!",
+                                        text:   "Por favor verifique sua caixa de E-mail e Spam!",
+                                        icon:   "success",
+                                        button: "OK",
+                                    }
+                                )   
+
+                            }
+                            else
+                            {
+                                passos_reset_email('2','Pendente');  
+                                swal(
+                                    {
+                                        title:  "Problema ao enviar e-mail!",
+                                        text:   "Por favor verificar e-mail informado!",
+                                        icon:   "error",
+                                        button: "OK",
+                                    }
+                                )                                   
+                            }
+                        }
+                    )
+                break; 
+        
+                case 'nao':
+                    swal(
+                        {
+                            title:  "Usuário não encontrado!",
+                            text:   "Por favor entrar em contato com o administrador do sistema!",
+                            icon:   "warning",
+                            button: "OK",
+                        }
+                    )                    
+                    // Marcar passo como Pendente
+                    passos_reset_email('2','Pendente');
+                    return false;               
+                    
+                default:
+                swal(
+                    {
+                        title:  "Problemas ao encontrar usuário!",
+                        text:   "Por favor entrar em contato com o administrador do sistema!",
+                        icon:   "error",
+                        button: "OK",
+                    }
+                )
+                passos_reset_email('2','Pendente');
+                return false;               
+            }              
         }
     )
-
-    // Marcar passo 1 como OK
-    passos_reset_email('2','OK');
 }
 
 // Marca os passos de resetar os e-mail como OK
