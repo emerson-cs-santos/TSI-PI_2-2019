736574e6ec8401c9b791589b977f0798d3df7010
Emerson Costa <syphonemerson@yahoo.com.br>
2019-10-29 13:40:46 -0300

    Filtro produto
    Modo de carregar tabela da consulta de produtos agora segue novo padrão com ajax
    Adicionado filtro para a consulta de produtos

--- JS/filtrar.js
+++ JS/filtrar.js
@@ -49,53 +49,53 @@ function filtrar_usuario()
    )
 }
 
-
 function filtrar_produto() 
 {  
-   var filtro = document.getElementById("produtos_filtro_status").value;
-   var where = '';
+    var where = '';
+   
+    var filtro_codigo       = document.getElementById("produtos_filtro_codigo").value;
+    var filtro_produto      = document.getElementById("produtos_filtro_nome").value;
+    var filtro_categoria    = document.getElementById("produtos_filtro_categoria").value;
+    var filtro_status       = document.getElementById("produtos_filtro_status").value;
+   
+    if(!filtro_codigo == '')
+    {
+        where = where + " and codigo = " + filtro_codigo;
+    }
 
-   switch (filtro)
+    if(!filtro_produto == '')
+    {
+        where = " and nome like " + "'%" + filtro_produto + "%'";
+    }  
+    
+    if(!filtro_categoria == '')
+    {
+        where = " and categoria like " + "'%" + filtro_categoria + "%'";
+    }        
+
+   switch (filtro_status)
    {
         case 'Ativos':
-            where = " where tipo ='Ativo' ";
+            where = where + " and tipo ='Ativo' ";
         break;
 
         case 'Inativos':
-            where = " where tipo ='Inativo' ";
-        break
-
-        default:
-            where = '';
+            where = where + " and tipo ='Inativo' ";
+        break;
    }
 
-   // Filtrar precisa abrir a página novamente, por isso não se usa o AJAX
-   // Neste caso chama a própria página de usuarios, mas passando da forma simples o modo POST (GET também funcionaria)
-   // Mas para chamar neste modo precisa de um form, então criamos um e adicionamos na arvore DOM e usamos ela para chamar o PHP
-   // Como se fosse um formulário que um usuário digitou e clicou em enviar (neste caso usamos o submit do proprio form).
-   var form = document.createElement('FORM');
-
-   // Para não ficar mostrando um mini form no canto da tela
-   // Apenas usamos o form para fazer o filtro, não precisamos mostrar esse form criado aqui na tela
-   form.hidden=true;
-
-   form.action = 'Produtos.php';
-   form.id='form_produtos_filtro';
-   form.method='POST';
+   // Tirando 1º and, é sempre colocado um and, pois não sabemos quais filtros serão utilizados
+   if(!where == '')
+   {
+        where = " Where " + where.substring(5);
+   } 
    
-   var condicao_filtro = document.createElement("INPUT");
-   condicao_filtro.id="";
-   condicao_filtro.name='condicao_produto';
-   condicao_filtro.value=where;
-   form.appendChild(condicao_filtro);
-
-   var opcao_usada = document.createElement("INPUT");
-   opcao_usada.id="";
-   opcao_usada.name='filtro_produto';
-   opcao_usada.value=filtro;
-   form.appendChild(opcao_usada);
-   
-   document.body.appendChild(form);
-   
-   form.submit();
+   var parametros = "filtro_produto=" + where;
+   // Ajax com Jquery e está refazendo apenas a tabela 
+   $.post('PHP/consulta_produtos.php',parametros, function(data)
+       {
+           $('#table').html(data);
+           
+       }
+   )
 }
