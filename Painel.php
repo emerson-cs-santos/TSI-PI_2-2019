cbf83e614de84b777f94e7f8ac975239a57593de
Emerson Costa <syphonemerson@yahoo.com.br>
2019-11-15 19:09:01 -0300

    Painel Responsivo

--- Painel.php
+++ Painel.php
@@ -8,9 +8,10 @@
 
             <main>
                 <section class='row justify-content-center'>
+                    
                     <div class='row mt-5'>
                         <div class='col-sm-12 col-md-6 m-auto'>
-                            <div class="card" style="width: 18rem;">
+                            <div class="card central_painel" style="width: 18rem;">
                             <a href="Usuarios.php"> <img class="card-img-top" src="Imagens/cadastro_usuario.png" alt="Cadastro de Usuários"> 
                                 <div class="card-body">
                                     <h2 class="card-title">Usuários</h2>
@@ -19,8 +20,8 @@
                             </div>
                         </div> 
 
-                        <div class='col-6'>
-                            <div class="card" style="width: 18rem;">
+                        <div class='col-sm-12 col-md-6 m-auto'>
+                            <div class="card central_painel" style="width: 18rem;">
                             <a href="Produtos.php"> <img class="card-img-top" src="Imagens/cadastro_produto.png" alt="Cadastro de Produtos">
                                 <div class="card-body">
                                     <h2 class="card-title">Produtos</h2>
